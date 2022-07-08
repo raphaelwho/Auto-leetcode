2598d79fba60f18ccb32321d5b08d7725d5ba621
raphaelwho <raphael.lhs@gmail.com>
2022-07-08 13:46:48 -0400

    fix bug

--- check.py
+++ check.py
@@ -5,5 +5,5 @@ past ={x[1]:x[2] for x in pd.read_csv('save.csv').values.tolist()}
 now = {x[0]:x[1] for x in pd.DataFrame.from_dict(get()).values.tolist()}
 
 for user in now:
-    if now[user]==past[user]:
+    if now[user]<=past[user]:
         print(user)
